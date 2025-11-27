7219b07b0e07cdc1c78a9e9c1e4c18c6bda55405
Talha Rana <mtalharana093@gmail.com>
2025-11-27 19:00:30 +0500

    all routes are defined for crud operation

--- app/main.py
+++ app/main.py
@@ -9,4 +9,35 @@ async def lifespan(app:FastAPI):
   yield
   
 app = FastAPI(lifespan=lifespan)
-  
+
+# Create Task
+@app.post("/task")
+def task_create(new_task: dict):
+  task = create_task(title=new_task["title"], content=new_task["content"])
+  return task
+
+
+# Get Single Task
+@app.get("/task{task_id}")
+def get_task(task_id: int):
+  task = get_task_by_id(task_id)
+  return task
+
+# Update Task
+@app.put("/task/{task_id}")
+def task_update(task_id: int, new_task: dict):
+  task = update_task(task_id, title=new_task["title"])
+  return task
+
+# Partial Update Task
+@app.patch("/task/{task_id}")
+def patch_task_update(task_id: int, new_task: dict):
+  task = patch_task(task_id, title=new_task.get("title"), content=new_task.get("content"))
+  return task
+
+
+# Delete Task
+@app.delete("/task/{task_id}")
+def delete(task_id: int):
+  task = delete_task(task_id)
+  return task
